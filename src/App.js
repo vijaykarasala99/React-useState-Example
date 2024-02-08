f3a4690b46784f86b47626167c2e6a49baac8d06
Vijay K <vijaykarasala99@gmail.com>
2024-02-08 16:13:22 +0530

    useEffect Updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import { Navbar, Cards } from "./navbar";
 import{BrowserRouter,Routes,Route,Link} from "react-router-dom";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 function App() {
   const[state,setState]=useState("HARSHA");
   const [count, setCount] = useState(0);
+   
+  const[data,setData]=useState([])
+  useEffect(()=>{
+      fetch('https://fakestoreapi.com/products')
+      .then(response=>response.json())
+      .then(value=>setData(value))
+  },[])
 
   function handleData() {
     setState("VIJAY"); 
   }
+
   return (
     <div className="App" style={{fontFamily:"cursive"}}>
      <h1 className="text-center">Vijay Karasala</h1>
-     
+      
+     {data.map((list)=>{
+      return(
+        <>
+        <li>{list.title}</li>
+        </>
+      )
+     }
+     )}
+
      <h2>{state}</h2>
      <button onClick={handleData}> UPDATE </button>
      <button onClick={() => setState("HARSHA")}>UNDO</button>
